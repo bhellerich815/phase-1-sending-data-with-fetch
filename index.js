89fd79c7ea5cb7b507880860a77a6cb89d38517a
Brooke Hellerich <brookeh815@cox.net>
2021-10-29 22:01:42 -0500

    commit tree b76b236c27061569deb500cf180795953cb34e09

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Add your code here
+
+const body = document.getElementsByTagName("body")[0];
+
+
+function submitData(firstName, email) {
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    };
+    return fetch("http://localhost:3000/users", configObject)
+    .then(response => response.json())
+    .then(body => addNewContact(body.id))
+    .catch((error) => {
+        addNewContact(error)
+    })
+}
+
+        // return fetch("http://localhost:3000/users", configObject)
+        //     .then(response => response.json())
+        //     .then((data) => {
+        //         addNewContact(data)
+        //     })
+        //     .catch((error) => {
+        //       addNewContact(error)
+        //     })
+        // }
+function addNewContact(newID){
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+}
+submitData("Name", "name@name.com");
